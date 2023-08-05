bc7803738a4489d73230281fa69ae0bd057493d0
salezzeczerniak <salezzeczerniak@gmail.com>
2023-08-04 21:55:21 -0300

    react+vsconnect

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import App from './App'
+import './index.css'
+import Home from './Pages/Home'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>,
+)
